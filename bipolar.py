f6d8b1403186a2271673c85eb22205957c82a09e
endolith <endolith@gmail.com>
2012-06-05 23:48:58 -0400

    Remove MATLAB comment marks from docstring

--- bipolar.py
+++ bipolar.py
@@ -6,45 +6,45 @@ from matplotlib import cm
 
 def bipolar(lutsize=256, n=0.333, interp=[]):
     """
-    %bipolar: symmetric/diverging/bipolar colormap, with neutral central color.
-    %
-    % Usage: cm = bipolar(m, neutral, interp)
-    %  neutral is the gray value for the middle of the colormap, default 1/3.
-    %  m is the number of rows in the colormap, defaulting to copy the current
-    %    colormap, or the colormap that MATLAB defaults for new figures.
-    %  interp is the method used to interpolate the colors, see interp1.
-    %
-    % The colormap goes from cyan-blue-neutral-red-yellow if neutral is < 0.5
-    % (the default) and from blue-cyan-neutral-yellow-red if neutral > 0.5.
-    %
-    % If neutral is exactly 0.5, then a map which yields a linear increase in
-    % intensity when converted to grayscale is produced (as derived in
-    % colormap_investigation.m). This colormap should also be reasonably good
-    % for colorblind viewers, as it avoids green and is predominantly based on
-    % the purple-yellow pairing which is easily discriminated by the two common
-    % types of colorblindness. For more details on this, see Brewer (1996):
-    % http://www.ingentaconnect.com/content/maney/caj/1996/00000033/00000002/art00002
-    % 
-    % Examples:
-    %  surf(peaks)
-    %  cmx = max(abs(get(gca, 'CLim')));
-    %  set(gca, 'CLim', [-cmx cmx]);
-    %  colormap(bipolar)
-    %
-    %  imagesc(linspace(-1, 1,201)) % symmetric data, if not set symmetric CLim
-    %  colormap(bipolar(201, 0.1)) % dark gray as neutral
-    %  axis off; colorbar
-    %  pause(2)
-    %  colormap(bipolar(201, 0.9)) % light gray as neutral
-    %  pause(2)
-    %  colormap(bipolar(201, 0.5)) % grayscale-friendly colormap
-    %
-    % See also: colormap, jet, interp1, colormap_investigation, dusk
-    % dusk is a colormap like bipolar(m, 0.5), in Oliver Woodford's real2rgb:
-    %  http://www.mathworks.com/matlabcentral/fileexchange/23342
-    %
-    % Copyright 2009 Ged Ridgway at gmail com
-    % Based on Manja Lehmann's hand-crafted colormap for cortical visualisation
+    bipolar: symmetric/diverging/bipolar colormap, with neutral central color.
+    
+     Usage: cm = bipolar(lutsize, n, interp)
+      n is the gray value for the neutral middle of the colormap, default 1/3.
+      m is the number of rows in the colormap, defaulting to copy the current
+        colormap, or the colormap that MATLAB defaults for new figures.
+      interp is the method used to interpolate the colors, see interp1.
+    
+     The colormap goes from cyan-blue-neutral-red-yellow if neutral is < 0.5
+     (the default) and from blue-cyan-neutral-yellow-red if neutral > 0.5.
+    
+     If neutral is exactly 0.5, then a map which yields a linear increase in
+     intensity when converted to grayscale is produced (as derived in
+     colormap_investigation.m). This colormap should also be reasonably good
+     for colorblind viewers, as it avoids green and is predominantly based on
+     the purple-yellow pairing which is easily discriminated by the two common
+     types of colorblindness. For more details on this, see Brewer (1996):
+     http://www.ingentaconnect.com/content/maney/caj/1996/00000033/00000002/art00002
+     
+     [matlab] Examples:
+      surf(peaks)
+      cmx = max(abs(get(gca, 'CLim')));
+      set(gca, 'CLim', [-cmx cmx]);
+      colormap(bipolar)
+    
+      imagesc(linspace(-1, 1,201)) % symmetric data, if not set symmetric CLim
+      colormap(bipolar(201, 0.1)) % dark gray as neutral
+      axis off; colorbar
+      pause(2)
+      colormap(bipolar(201, 0.9)) % light gray as neutral
+      pause(2)
+      colormap(bipolar(201, 0.5)) % grayscale-friendly colormap
+    
+     See also: colormap, jet, interp1, colormap_investigation, dusk
+     dusk is a colormap like bipolar(m, 0.5), in Oliver Woodford's real2rgb:
+      http://www.mathworks.com/matlabcentral/fileexchange/23342
+    
+     Copyright 2009 Ged Ridgway at gmail com
+     Based on Manja Lehmann's hand-crafted colormap for cortical visualisation
     
     """
     if n < 0.5:
@@ -96,12 +96,8 @@ if __name__ == "__main__":
     x = arange(-3.0, 3.0001, dx)
     y = arange(-3.0, 3.0001, dy)
     X,Y = meshgrid(x, y)
-    
-    #plot the colorbar with luminance, then adjust it to the opposite of coolwarm
-    
+       
     Z = func3(X, Y)
-    #imshow(Z, vmax=abs(Z).max(), vmin=-abs(Z).max(), cmap=bipolar(n=1./3, interp='linear'))
-    #figure()
     pcolor(X, Y, Z, cmap=bipolar(n=1./3, interp='linear'), vmax=abs(Z).max(), vmin=-abs(Z).max())
     colorbar()
     axis([-3,3,-3,3])
